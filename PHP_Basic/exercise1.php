9a747a5b3c970d5eae92abb10211703ef6c23a11
vunguyenkhanh <khanh.vn176795@sis.hust.edu.vn>
2021-09-30 16:25:21 +0700

    Add input param

--- PHP_Basic/exercise1.php
+++ PHP_Basic/exercise1.php
@@ -1,20 +1,18 @@
 <?php
-    function checkValidString($str)
+    function checkValidString($str, $str1, $str2)
     {
-        $checkBook = strpos($str, 'book');
-        $checkRes = strpos($str, 'restaurant');
-        if (($checkBook > 0 && $checkRes == false) || ($checkBook == false && $checkRes > 0)) {
+        if ((strpos($str, $str1) > 0 && strpos($str, $str2) == false) || (strpos($str, $str1) == false && strpos($str, $str2) > 0)) {
             return true;
         }
         return false;   
     }
 
-    function checkFile($filename)
+    function checkFile($filename, $str1, $str2)
     {
         $file = fopen($filename, 'r') or die(("Không thể mở file!"));
         $content = fread($file, filesize($filename));
 
-        if (checkValidString($content)) {
+        if (checkValidString($content, $str1, $str2)) {
             $sentence = substr_count($content, '.');
             echo 'Chỗi hợp lệ. Chuỗi bao gồm ' . $sentence . ' câu';
         } else {
@@ -22,7 +20,7 @@
         }
     }
 
-    checkFile('file1.txt');
+    checkFile('file1.txt', 'restaurant', 'book');
     echo '<br>';
-    checkFile('file2.txt');
+    checkFile('file2.txt', 'restaurant', 'book');
 ?>
