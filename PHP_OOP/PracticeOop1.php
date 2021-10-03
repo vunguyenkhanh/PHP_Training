de86f2402c9131fbb9195ed2dae466a93df12974
vunguyenkhanh <khanh.vn176795@sis.hust.edu.vn>
2021-10-03 19:59:09 +0700

    Fix OOP1

--- PHP_OOP/PracticeOop1.php
+++ PHP_OOP/PracticeOop1.php
@@ -9,14 +9,13 @@ class ExerciseString
         $file = fopen($filename, 'r') or die(("Không thể mở file!"));
         $content = fread($file, filesize($filename));
         fclose($file);
+
         return $content;
     }
 
-    public function checkValidString($str)
+    public function checkValidString($str, $str1, $str2)
     {
-        $checkBook = strpos($str, 'book');
-        $checkRes = strpos($str, 'restaurant');
-        if (($checkBook > 0 && $checkRes == false) || ($checkBook == false && $checkRes > 0)) {
+        if ((strpos($str, $str1) > 0 && strpos($str, $str2) == false) || (strpos($str, $str1) == false && strpos($str, $str2) > 0)) {
             return true;
         }
         return false;   
@@ -27,19 +26,20 @@ class ExerciseString
         $file = fopen($filename, 'w') or die(("Không thể mở file!"));
         fwrite($file, $str);
         fclose($file);
+
     }
 }
     
 $object1 = new ExerciseString();
 
 $content1 = $object1->readFile("file1.txt");
-$object1->checkValidString($content1);
-$object1->check1 = $object1->checkValidString($content1);
+$object1->checkValidString($content1,'restaurant','book');
+$object1->check1 = $object1->checkValidString($content1,'restaurant','book');
 
 $content2 = $object1->readFile("file2.txt");
 $sentence = substr_count($content2, ".");
-$object1->checkValidString($content2);
-$object1->check2 = $object1->checkValidString($content2);
+$object1->checkValidString($content2,'restaurant','book');
+$object1->check2 = $object1->checkValidString($content2,'restaurant','book');
 
 $checkFile1 = ($object1->check1 == true) ? 'check1 là chuỗi Hợp lệ' : 'check1 là chuỗi Không hợp lệ';
 $checkFile2 = ($object1->check2 == true) ? 'check2 là chuỗi Hợp lệ. Chuỗi có ' . $sentence . ' câu.' : 'check2 là chuỗi Không hợp lệ. Chuỗi có ' . $sentence . ' câu.';
